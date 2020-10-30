ebffc8730f5a0d3f6a129fd676f47e0d2e2fdc72
63070096 <73691040+63070096@users.noreply.github.com>
2020-10-30 19:04:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
             <h1>My นายบารเมษฐ สายจันยูร/h1>
             <p>ชื่อเล่น ม่อน>< </p>
             <img src="chino.jpg">
-            <img src="Thunderer.png">
+            <img src="queenelizabeth.jpg">
             <br>
             <a href="https://www.thairath.co.th">This is my favorite to read the politice news</a>
             <h2>The unordered list</h2>
