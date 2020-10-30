5251e6658a044e9c574d9684ef684c6d661b8af3
63070096 <73691040+63070096@users.noreply.github.com>
2020-10-30 18:59:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
         <tittle>hello world</tittle>
         <\head>
         <body>
-            <h1>My First Heading</h1>
-            <p>My First Paragraph</p>
+            <h1>My นายบารเมษฐ สายจันยูร/h1>
+            <p>ชื่อเล่น ม่อน>< </p>
             <img src="chino.jpg">
+            <img src="Thunderer.png">
             <br>
             <a href="https://www.thairath.co.th">This is my favorite to read the politice news</a>
             <h2>The unordered list</h2>
