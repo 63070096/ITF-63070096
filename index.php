6cf998cf4b6223c33a25658a56e4f549a9263702
63070096 <73691040+63070096@users.noreply.github.com>
2020-11-19 19:25:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
     }
     $res = mysqli_query($conn, 'SELECT * FROM guestbook');
     ?>
+    <table width="600" border="1">
     <table class="table table-dark table-striped">
       <tr>
         <th width="100"> <div align="left">Name</div></th>
