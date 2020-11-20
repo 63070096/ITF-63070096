6f9d8e5f9d98760fe82a0320582f59d07f8903ef
63070096 <73691040+63070096@users.noreply.github.com>
2020-11-20 22:59:18 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'localhost', 'root', '', 'itflab', 3306);
+mysqli_real_connect($conn, 'itf-63070096.mysql.database.azure.com', 'it63070096@itf-63070096', 'DTSquj72', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
