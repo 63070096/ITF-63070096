1a2cd9a40901a5772d8ead0fa8584b80c71efa6b
63070096 <73691040+63070096@users.noreply.github.com>
2020-11-20 22:51:46 +0700

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'itf-63070096.mysql.database.azure.com', 'it63070096@itf-63070096', 'DTSquj72', 'itflab', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-?>
